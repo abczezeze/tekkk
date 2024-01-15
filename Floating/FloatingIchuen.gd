f29b006678af04650d42ae84218702cf0cf4a9da
ABCzezeze <archueyouler@gmail.com>
2024-01-15 22:11:30 +0700

    set visible label

---
+++ Floating/FloatingIchuen.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+func _ready():
+	Global.FloatingPlay()
+	$AnimatedSprite.speed_scale=0
+	$Click_.text = str(Global.ichuen_scores)
+	
+	for n in 14:
+		var ichuen_add = $ichuen_right.duplicate()
+		add_child(ichuen_add)
+	
+func _process(delta):
+	if $ichuen_right.hit_ichuen == true :
+		press_right()
+	
+	$ichuen_right.hit_ichuen = false 
+	
+func _on_HomeBT_pressed():
+	Global.FloatingStop()
+	Global.HomeAudioPlay()
+	Global.MenuAudioP()
+	get_tree().change_scene("res://Scene/MainMenu.tscn")
+
+func press_right():
+	Global.ichuen_scores += 1
+	$Click_.text = str(Global.ichuen_scores)
+	$AnimatedSprite.speed_scale += 1
+	Global.TurntableP()
+	if $AnimatedSprite.speed_scale>=10:
+		_on_HomeBT_pressed()
+func _on_Timer_timeout():
+	$HBoxContainer/Label.visible = false
---
+++ Floating/FloatingOlay.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+func _ready():
+	Global.FloatingPlay()
+	$Click_.text = str(Global.olay_scores)
+	$AnimatedSprite.speed_scale=0
+	for n in 14:
+		var olay_add = $olay_right.duplicate()
+		add_child(olay_add)
+	
+func _process(delta):
+	if $olay_right.hit_olay == true :
+		press_right()
+	
+	$olay_right.hit_olay = false 
+
+func _on_HomeBT_pressed():
+	Global.FloatingStop()
+	Global.HomeAudioPlay()
+	Global.MenuAudioP()
+	get_tree().change_scene("res://Scene/MainMenu.tscn")
+	
+func press_right():
+	Global.olay_scores += 1
+	$Click_.text = str(Global.olay_scores)
+	$AnimatedSprite.speed_scale += 1
+	if $AnimatedSprite.speed_scale>=10:
+		_on_HomeBT_pressed()
+func _on_Timer_timeout():
+	$Label.visible = false
