821610739535698c923cbd63711e89d01d883b10
ABCzezeze <archueyouler@gmail.com>
2024-01-18 15:04:47 +0700

    move Language to mainmenu

--- Scene/MainMenu.gd
+++ Scene/MainMenu.gd
@@ -2,12 +2,7 @@ extends Control
 
 func _ready():
 	Global.MenuAudioP()
-	if Global.tekkk_language == "Th":
-		$VBoxContainer/TekLb.text="เต็กเคเค"
-		$VBoxContainer2/SelectLevel.text="เลือกด่าน"
-		$VBoxContainer2/HBoxContainer/ClickLb.text="คลิก"
-		$VBoxContainer2/HBoxContainer3/DragLb.text="ลาก"
-		$VBoxContainer2/HBoxContainer2/SwipeLb.text="ตวัด"
+	$VBoxContainer2/VBoxContainer/EnglishBt.disabled=true
 
 func _on_ClickLb_pressed():
 	Global.MenuAudioS()
@@ -42,7 +37,28 @@ func _on_lv4_pressed():
 	Global.SceneSoundPlay()
 	get_tree().change_scene("res://Scene/Swipe/SwipeMix.tscn")
 
+func _on_EnglishBt_pressed():
+	$VBoxContainer2/VBoxContainer/EnglishBt.disabled=true
+	$VBoxContainer2/VBoxContainer/ThailandBt.disabled=false
+	Global.tekkk_language = "En"
+	$TekLb.text="Tekkk"
+	$VBoxContainer2/SelectLevel.text="Select Level"
+	$VBoxContainer2/HBoxContainer/ClickLb.text="Click"
+	$VBoxContainer2/HBoxContainer3/DragLb.text="Drag"
+	$VBoxContainer2/HBoxContainer2/SwipeLb.text="Swipe"
+	$VBoxContainer2/VBoxContainer/Language.text="Language"
+	$VBoxContainer2/VBoxContainer/EnglishBt.text="English"
+	$VBoxContainer2/VBoxContainer/ThailandBt.text="Thailand"
 
-
-
-
+func _on_ThailandBt_pressed():
+	$VBoxContainer2/VBoxContainer/EnglishBt.disabled=false
+	$VBoxContainer2/VBoxContainer/ThailandBt.disabled=true
+	Global.tekkk_language = "Th"
+	$TekLb.text="เต็กเคเค"
+	$VBoxContainer2/SelectLevel.text="เลือกด่าน"
+	$VBoxContainer2/HBoxContainer/ClickLb.text="คลิก"
+	$VBoxContainer2/HBoxContainer3/DragLb.text="ลาก"
+	$VBoxContainer2/HBoxContainer2/SwipeLb.text="ตวัด"
+	$VBoxContainer2/VBoxContainer/Language.text="ภาษา"
+	$VBoxContainer2/VBoxContainer/EnglishBt.text="อังกฤษ"
+	$VBoxContainer2/VBoxContainer/ThailandBt.text="ไทย"
