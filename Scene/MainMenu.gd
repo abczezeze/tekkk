9b1c47ed99209e9f7af916d6293ada91e3f4af2f
ABCzezeze <archueyouler@gmail.com>
2024-06-18 10:00:16 +0700

    change language toggle button

--- Scene/MainMenu.gd
+++ Scene/MainMenu.gd
@@ -6,47 +6,9 @@ func _ready() -> void:
 	$LogoNocap.visible = true
 	$AnimationPlayer.play("scale_logo")
 	if Global.tekkk_language=="En":
-		_on_EnglishBt_pressed()
+		_on_TextureButton_toggled(false)
 	if Global.tekkk_language=="Th":
-		_on_ThailandBt_pressed()
-
-func _on_EnglishBt_pressed() -> void:
-	$VBoxContainer2/VBoxContainer/EnglishBt.disabled=true
-	$VBoxContainer2/VBoxContainer/ThailandBt.disabled=false
-	Global.tekkk_language = "En"
-	$TekLb.text="Tekkk"
-	$VBoxContainer2/SelectLevel.text="Select Level"
-	$VBoxContainer2/HBoxContainer/ClickLb.text="Click"
-	$VBoxContainer2/HBoxContainer/DragLb.text="Drag"
-	$VBoxContainer2/HBoxContainer/SwipeLb.text="Swipe"
-	$VBoxContainer2/level_3d/click_3d.text="click"
-	$VBoxContainer2/level_3d/drag_3d.text="drag"
-	$VBoxContainer2/level_3d/shoot_3d.text="shoot"
-	$VBoxContainer2/VBoxContainer/Language.text="Language"
-	$VBoxContainer2/VBoxContainer/EnglishBt.text="English"
-	$VBoxContainer2/VBoxContainer/ThailandBt.text="Thailand"
-	$VBoxContainer2/OtherLb.text="Other"
-	$VBoxContainer2/DetailBt.text="Detail"
-	$VBoxContainer2/reset_button.text="RESET!"
-
-func _on_ThailandBt_pressed() -> void:
-	$VBoxContainer2/VBoxContainer/EnglishBt.disabled=false
-	$VBoxContainer2/VBoxContainer/ThailandBt.disabled=true
-	Global.tekkk_language = "Th"
-	$TekLb.text="เต็กเคเค"
-	$VBoxContainer2/SelectLevel.text="เลือกด่าน"
-	$VBoxContainer2/HBoxContainer/ClickLb.text="คลิก"
-	$VBoxContainer2/HBoxContainer/DragLb.text="ลาก"
-	$VBoxContainer2/HBoxContainer/SwipeLb.text="ตวัด"
-	$VBoxContainer2/level_3d/click_3d.text="คลิก"
-	$VBoxContainer2/level_3d/drag_3d.text="ลาก"
-	$VBoxContainer2/level_3d/shoot_3d.text=" ยิง "
-	$VBoxContainer2/VBoxContainer/Language.text="ภาษา"
-	$VBoxContainer2/VBoxContainer/EnglishBt.text="อังกฤษ"
-	$VBoxContainer2/VBoxContainer/ThailandBt.text="ไทย"
-	$VBoxContainer2/OtherLb.text="อื่นๆ"
-	$VBoxContainer2/DetailBt.text="รายละเอียด"
-	$VBoxContainer2/reset_button.text="เริ่มค่าใหม่!"
+		_on_TextureButton_toggled(true)
 
 func _on_SwipeLb_pressed() -> void:
 	Global.SceneSoundPlay()
@@ -92,3 +54,35 @@ func _on_reset_button_pressed() -> void:
 	Global.save_dict["speng_anim"]=false
 	Global.save_game()
 	Global.load_game()
+
+
+func _on_TextureButton_toggled(button_pressed):
+	print(button_pressed)
+	if button_pressed:
+		$TextureButton_language.modulate = Color(1,1,1,2)
+		Global.tekkk_language = "Th"
+		$TekLb.text="เต็กเคเค"
+		$VBoxContainer2/SelectLevel.text="เลือกด่าน"
+		$VBoxContainer2/HBoxContainer/ClickLb.text="คลิก"
+		$VBoxContainer2/HBoxContainer/DragLb.text="ลาก"
+		$VBoxContainer2/HBoxContainer/SwipeLb.text="ตวัด"
+		$VBoxContainer2/level_3d/click_3d.text="คลิก"
+		$VBoxContainer2/level_3d/drag_3d.text="ลาก"
+		$VBoxContainer2/level_3d/shoot_3d.text=" ยิง "
+		$VBoxContainer2/OtherLb.text="อื่นๆ"
+		$VBoxContainer2/DetailBt.text="รายละเอียด"
+		$VBoxContainer2/reset_button.text="เริ่มค่าใหม่!"
+	else:
+		$TextureButton_language.modulate = Color(1,1,1,1)
+		Global.tekkk_language = "En"
+		$TekLb.text="Tekkk"
+		$VBoxContainer2/SelectLevel.text="Select Level"
+		$VBoxContainer2/HBoxContainer/ClickLb.text="Click"
+		$VBoxContainer2/HBoxContainer/DragLb.text="Drag"
+		$VBoxContainer2/HBoxContainer/SwipeLb.text="Swipe"
+		$VBoxContainer2/level_3d/click_3d.text="click"
+		$VBoxContainer2/level_3d/drag_3d.text="drag"
+		$VBoxContainer2/level_3d/shoot_3d.text="shoot"
+		$VBoxContainer2/OtherLb.text="Other"
+		$VBoxContainer2/DetailBt.text="Detail"
+		$VBoxContainer2/reset_button.text="RESET!"
