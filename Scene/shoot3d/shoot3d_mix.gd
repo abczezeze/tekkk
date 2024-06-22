ab3b84227ddcf8b2e0d3a71db9da62767bdcd66f
ABCzezeze <archueyouler@gmail.com>
2024-06-23 06:00:43 +0700

    set linear velocity or physic body

--- Scene/shoot3d/shoot3d_mix.gd
+++ Scene/shoot3d/shoot3d_mix.gd
@@ -8,18 +8,26 @@ var rigidbody_drum:RigidBody
 var rigidbody_bass_c = preload("res://Scene/drag3d/rigidbody_bass.tscn")
 var rigidbody_bass:RigidBody
 
-var mouse_positon = Vector2()
-var mouse_sens= 300.0
-var mouse_speed = 5
-
+var mouse_positon:Vector2 = Vector2()
+var speed:int = 10
+var time_to_quere_free:float = 0
 func _ready():
 	pass
 
 func _physics_process(delta):
+	time_to_quere_free+=delta
+	print(time_to_quere_free)
+	mouse_positon = get_viewport().get_mouse_position()
 	var from = $Camera.project_ray_origin(mouse_positon)
 	var to = from + $Camera.project_ray_normal(mouse_positon) * 10
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_click"):
 		rigidbody_turntable = rigidbody_turntable_c.instance()
-		rigidbody_turntable.global_transform.origin = to
+		rigidbody_turntable.transform.origin = to
 		self.add_child(rigidbody_turntable)
+#		rigidbody_turntable.apply_central_impulse(Vector3(0,0,-100))
+		rigidbody_turntable.linear_velocity.z = -10
+#		print(rigidbody_turntable.translation.z)
+		if time_to_quere_free>5:
+			rigidbody_turntable.queue_free()
+			time_to_quere_free = 0
 		
