44473d9a76001d929034b50ecc0cdf1f9e3d8ad3
ABCzezeze <archueyouler@gmail.com>
2024-06-26 22:08:35 +0700

    fix 626
    -add button main scene
    -instance player
    -check valid instance musical

--- Scene/shoot3d/shoot3d_mix.gd
+++ Scene/shoot3d/shoot3d_mix.gd
@@ -1,4 +1,5 @@
 extends Spatial
+
 var rigidbody_turntable_c = preload("res://Scene/drag3d/rigidbody_turntable.tscn")
 var rigidbody_turntable:RigidBody
 var rigidbody_guitar_c = preload("res://Scene/drag3d/rigidbody_guitar.tscn")
@@ -12,89 +13,69 @@ var player_ichuen_head_rigid_c = preload("res://Scene/shoot3d/player_ichuen_head
 var player_ichuen_head_rigid:RigidBody
 var animation_ichuen:AnimationPlayer
 var label_ichuen:Label3D
+var sfx_ichuen:AudioStreamPlayer
+var paticle_ichuen:CPUParticles
 
-var dir_x_ichuen:float = 0.1
-var dir_y_ichuen:float = 0.1
-var move_speed_ichuen:float = 1
-var clicked_ichuen:bool = false
 export (Array,AudioStreamOGGVorbis) var sound_efx_ichuen = []
+export (Array,AudioStreamOGGVorbis) var sound_efx_mno = []
 
 var mouse_positon:Vector2 = Vector2()
 var speed:int = 10
-var time_to_quere_free:float = 0
-var timeMovePathFollow:float = 0
-
-var dir_x_mno:float = 0.1
-var move_speed_mno:float = 5
-var clicked_mno:bool = false
-export (Array,AudioStreamOGGVorbis) var sound_efx_mno = []
 
 func _ready():
 	new_player_ichue_head_rigid()
 	
-#	$player_ichuen_head/AnimationPlayer.play("idle")
-#	dir_x_ichuen = rand_range(-1,1)
-#	dir_y_ichuen = rand_range(-1,1)
-#	$player_ichuen_head/Label3D.text = str(Global.save_dict["ichuen_scores"])
-##	$player_ichuen_head.transform.origin.x = rand_range(-2,2)
-#	var __1 = $player_ichuen_head/AnimationPlayer.connect("animation_finished",self,"_on_AnimationPlayer_ichuen")
-	
-#	$player_mno_head/AnimationPlayer.play("idle")
-#	dir_x_mno = rand_range(-2,2)
-#	$player_mno_head/Label3D.text = str(Global.save_dict["mno_scores"])
-#	$player_mno_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),-2)
-#	var __2 = $player_mno_head/AnimationPlayer.connect("animation_finished",self,"_on_AnimationPlayer_mno")
-	
-func _on_AnimationPlayer_ichuen(_anim_name):
-	$player_ichuen_head/ICBd/CPUParticles.emitting = false
-	clicked_ichuen = false
-	$player_ichuen_head/AnimationPlayer.play("idle")
-	
-#func _on_AnimationPlayer_mno(_anim_name):
-#	$player_mno_head/mnoTrouser/CPUParticles.emitting = false
-#	clicked_mno = false
-#	$player_mno_head/AnimationPlayer.play("idle")
-
 func _physics_process(delta):
-	
 	mouse_positon = get_viewport().get_mouse_position()
 	var origin = $Camera.project_ray_origin(mouse_positon)
-	var end = origin + $Camera.project_ray_normal(mouse_positon) * 5
+	var end = origin + $Camera.project_ray_normal(mouse_positon) * 3
 	
 	if Input.is_action_just_pressed("ui_click"):
 #		var depth = origin.distance_to($player_ichuen_head.transform.origin)
 #		var final_pos = origin + end * depth
 		rigidbody_turntable = rigidbody_turntable_c.instance()
+		
 		rigidbody_turntable.global_transform.origin = end
 		add_child(rigidbody_turntable)
 		var forward = rigidbody_turntable.get_global_transform().basis.z
 		forward*=-1
 		rigidbody_turntable.apply_impulse(forward,forward*50)
-#		print(rigidbody_turntable.get_global_transform().basis.z)
+		
+	if is_instance_valid(rigidbody_turntable):
+		if rigidbody_turntable.global_transform.origin.z < -20:
+			rigidbody_turntable.queue_free()
 
 	if player_ichuen_head_rigid.translation.y < -13 or player_ichuen_head_rigid.translation.z < -20:
 		player_ichuen_head_rigid.queue_free()
 		new_player_ichue_head_rigid()
 	
-	label_ichuen.text = str(Global.save_dict["ichuen_scores"])
-	
-#	if has_rigidbody_turntable:
-#		if rigidbody_turntable.transform.origin.z < -20:
-#			rigidbody_turntable.queue_free()
-		
+#	label_ichuen.text = str(Global.save_dict["ichuen_scores"])
+
 func _on_player_ichuen_head_body_entered(body):
 	print(body)
 	if body.is_in_group("turntable"):
 		Global.save_dict["ichuen_scores"]+=1
-		rigidbody_turntable.queue_free()
-#		$player_ichuen_head/ICBd/CPUParticles.emitting = true
-		
+		paticle_ichuen.emitting = false
+		animation_ichuen.play("head_rotation")
+
+func _on_AnimationPlayer_ichuen(_anim_name):
+	paticle_ichuen.emitting = false
+	animation_ichuen.play("idle")
+
 func new_player_ichue_head_rigid():
 	player_ichuen_head_rigid = player_ichuen_head_rigid_c.instance()
 	add_child(player_ichuen_head_rigid)
-	var __ = player_ichuen_head_rigid.connect("body_entered",self,"_on_player_ichuen_head_body_entered")
-	player_ichuen_head_rigid.transform.origin = Vector3(rand_range(-2,2),5,-10)
+	player_ichuen_head_rigid.transform.origin = Vector3(rand_range(-2,2),5,-7)
+	label_ichuen = player_ichuen_head_rigid.get_node("Label3D")
+	paticle_ichuen = player_ichuen_head_rigid.get_node("ICBd/CPUParticles")
 	animation_ichuen = player_ichuen_head_rigid.get_node("AnimationPlayer")
 	animation_ichuen.play("idle")
-#	animation_ichuen.connect("animation_finished",self,"_on_AnimationPlayer_ichuen")
-	label_ichuen = player_ichuen_head_rigid.get_node("Label3D")
+	var __2 = animation_ichuen.connect("animation_finished",self,"_on_AnimationPlayer_ichuen")
+	var __ = player_ichuen_head_rigid.connect("body_entered",self,"_on_player_ichuen_head_body_entered")
+	
+
+
+func _on_home_button_pressed():
+	Global.HomeAudioPlay()
+	Global.MenuAudioP()
+	var __ = get_tree().change_scene("res://Scene/MainMenu.tscn")
