d59074ce1a6b6cb405f8b8ee9a680e0f8a01101f
ABCzezeze <archueyouler@gmail.com>
2024-07-05 12:07:26 +0700

    variable

--- Scene/click3d/click_mix_3d.gd
+++ Scene/click3d/click_mix_3d.gd
@@ -23,7 +23,7 @@ export (Array, Texture) var sprite_textures = []
 func _ready():
 	$player_speng_head/AnimationPlayer.play("idle")
 	$player_speng_head/Label3D.text = str(Global.save_dict["speng_scores"])
-	$player_speng_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),-21)
+	$player_speng_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),-21)
 	var __0 = $player_speng_head.connect("input_event",self,"_on_player_speng_head_input_event")
 	var __1 = $player_speng_head/AnimationPlayer.connect("animation_finished",self,"_on_AnimationPlayer_speng")
 	var __2 = $player_speng_head/Timer.connect("timeout",self,"_on_Timer_timeout")
@@ -34,21 +34,21 @@ func _ready():
 	dir_x_ichuen = rand_range(-1,1)
 	dir_y_ichuen = rand_range(-1,1)
 	$player_ichuen_head/Label3D.text = str(Global.save_dict["ichuen_scores"])
-	$player_ichuen_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),0)
+	$player_ichuen_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),0)
 	var __3 = $player_ichuen_head.connect("input_event",self,"_on_player_ichuen_head_input_event")
 	var __4 = $player_ichuen_head/AnimationPlayer.connect("animation_finished",self,"_on_AnimationPlayer_ichuen")
 	
 	$player_mno_head/AnimationPlayer.play("idle")
-	dir_x_mno = rand_range(-2,2)
+	dir_x_mno = rand_range(-1,1)
 	$player_mno_head/Label3D.text = str(Global.save_dict["mno_scores"])
-	$player_mno_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),-6)
+	$player_mno_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),-6)
 	var __5 = $player_mno_head.connect("input_event",self,"_on_player_mno_head_input_event")
 	var __6 = $player_mno_head/AnimationPlayer.connect("animation_finished",self,"_on_AnimationPlayer_mno")
 	
 	$player_olay_head/AnimationPlayer.play("idle")
-	dir_y_olay = rand_range(-2,2)
+	dir_y_olay = rand_range(-1,1)
 	$player_olay_head/Label3D.text = str(Global.save_dict["olay_scores"])
-	$player_olay_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),-14)
+	$player_olay_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),-14)
 	var __7 = $player_olay_head.connect("input_event",self,"_on_player_olay_head_input_event")
 	var __8 = $player_olay_head/AnimationPlayer.connect("animation_finished",self,"_on_AnimationPlayer_olay")
 		
@@ -71,40 +71,43 @@ func _process(delta):
 		$VBoxContainer/TextureButton_down.disabled = false
 		$WorldEnvironment.environment.background_sky.sky_top_color = Color(0.95,1,0,1)
 	else:
-		$WorldEnvironment.environment.background_sky.sky_top_color = Color(0,0,0,1)
 		$VBoxContainer/TextureButton_up.disabled = true
 		$VBoxContainer/TextureButton_down.disabled = true
+		$WorldEnvironment.environment.background_sky.sky_top_color = Color(0,0,0,1)
+		
 	#ichuen move
 	$player_ichuen_head.translation += Vector3(dir_x_ichuen*move_speed_ichuen*delta,dir_y_ichuen*move_speed_ichuen*delta,0)
-	if $player_ichuen_head.transform.origin.x>2:
+	if $player_ichuen_head.transform.origin.x>7:
 		dir_x_ichuen = rand_range(-1,0)
-		$player_ichuen_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),0)
-	elif $player_ichuen_head.transform.origin.x<-2:
+		$player_ichuen_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),0)
+	elif $player_ichuen_head.transform.origin.x<-7:
 		dir_x_ichuen = rand_range(0,1)
-		$player_ichuen_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),0)
+		$player_ichuen_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),0)
 		
 	if $player_ichuen_head.transform.origin.y>4:
 		dir_y_ichuen = rand_range(-1,0)
-		$player_ichuen_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),0)
+		$player_ichuen_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),0)
 	elif $player_ichuen_head.transform.origin.y<-4:
 		dir_y_ichuen = rand_range(0,1)
-		$player_ichuen_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),0)
+		$player_ichuen_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),0)
 	#mno move
 	$player_mno_head.translation.x += dir_x_mno*move_speed_mno*delta
-	if $player_mno_head.transform.origin.x>2:
+	if $player_mno_head.transform.origin.x>7:
 		dir_x_mno = rand_range(-2,0)
-		$player_mno_head.transform.origin = Vector3(1,rand_range(-3,3),-6)
-	elif $player_mno_head.transform.origin.x<-2:
+		$player_mno_head.transform.origin = Vector3(1,rand_range(-4,4),-6)
+	elif $player_mno_head.transform.origin.x<-7:
 		dir_x_mno = rand_range(0,2)
-		$player_mno_head.transform.origin = Vector3(-1,rand_range(-3,3),-6)
+		$player_mno_head.transform.origin = Vector3(-1,rand_range(-4,4),-6)
 	#olay move
 	$player_olay_head.translation.y += dir_y_olay*move_speed_olay*delta
 	if $player_olay_head.transform.origin.y>4:
 		dir_y_olay = rand_range(-2,0)
-		$player_olay_head.transform.origin = Vector3(rand_range(-2,2),4,-14)
+		$player_olay_head.transform.origin = Vector3(rand_range(-7,7),4,-14)
 	elif $player_olay_head.transform.origin.y<-4:
 		dir_y_olay = rand_range(0,2)
-		$player_olay_head.transform.origin = Vector3(rand_range(-2,2),-4,-14)
+		$player_olay_head.transform.origin = Vector3(rand_range(-7,7),-4,-14)
+		
+	$HBoxContainer/TotalScores.text = str(Global.total_score)
 
 func _on_AnimationPlayer_ichuen(_anim_name):
 	$player_ichuen_head/ICBd/CPUParticles.emitting = false
@@ -130,7 +133,7 @@ func _on_player_ichuen_head_input_event(_camera, event, _position, _normal, _sha
 			Global.save_dict["ichuen_scores"]+=1
 			Global.save_game()
 			$player_ichuen_head/ICBd/CPUParticles.emitting = true
-			$player_ichuen_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),0)
+			$player_ichuen_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),0)
 			$player_ichuen_head/Label3D.text = str(Global.save_dict["ichuen_scores"])
 			$player_ichuen_head/AudioStreamPlayer.stream = sound_efx_ichuen[rand_range(0,sound_efx_ichuen.size())]
 			$player_ichuen_head/AudioStreamPlayer.stream.loop = false
@@ -145,7 +148,7 @@ func _on_player_mno_head_input_event(_camera, event, _position, _normal, _shape_
 			Global.save_dict["mno_scores"]+=1
 			Global.save_game()
 			$player_mno_head/mnoTrouser/CPUParticles.emitting = true
-			$player_mno_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),-6)
+			$player_mno_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),-6)
 			$player_mno_head/Label3D.text = str(Global.save_dict["mno_scores"])
 			$player_mno_head/AudioStreamPlayer.stream = sound_efx_mno[rand_range(0,sound_efx_mno.size())]
 			$player_mno_head/AudioStreamPlayer.stream.loop = false
@@ -160,7 +163,7 @@ func _on_player_olay_head_input_event(_camera, event, _position, _normal, _shape
 			Global.save_dict["olay_scores"]+=1
 			Global.save_game()
 			$player_olay_head/OlayCap/CPUParticles.emitting = true
-			$player_olay_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),-14)
+			$player_olay_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),-14)
 			$player_olay_head/Label3D.text = str(Global.save_dict["olay_scores"])
 			$player_olay_head/AudioStreamPlayer.stream = sound_efx_olay[rand_range(0,sound_efx_olay.size())]
 			$player_olay_head/AudioStreamPlayer.stream.loop = false
@@ -177,7 +180,7 @@ func _on_player_speng_head_input_event(_camera, event, _position, _normal, _shap
 			Global.save_dict["speng_scores"]+=1
 			Global.save_game()
 			$player_speng_head/SpeangBody/CPUParticles.emitting = true
-			$player_speng_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),-21)
+			$player_speng_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),-21)
 			$player_speng_head/Label3D.text = str(Global.save_dict["speng_scores"])
 			$player_speng_head/AudioStreamPlayer.stream = sound_efx_speng[rand_range(0,sound_efx_speng.size())]
 			$player_speng_head/AudioStreamPlayer.stream.loop = false
@@ -189,7 +192,7 @@ func _on_AnimationPlayer_speng(_anim_name):
 	$player_speng_head/AnimationPlayer.play("idle")
 
 func _on_Timer_timeout():
-	$player_speng_head.transform.origin = Vector3(rand_range(-2,2),rand_range(-4,4),-21)
+	$player_speng_head.transform.origin = Vector3(rand_range(-7,7),rand_range(-4,4),-21)
 		
 func _on_HomeBT_pressed():
 	var __ = get_tree().change_scene("res://Scene/MainMenu.tscn")
